bc8ade362a860c1c9c77afbfe4ad5f67ff39ad64
IlyaMahnach <ilya_mahnach@mail.ru>
2022-02-08 22:12:48 +0700

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,13 @@ public class Main {
     public static void main(String[] args) {
         // входные данные
         int price = 20000;
-        int bid = 20
+        int bid = 20;
+        // логика
         int bonus = price / bid;
+        System.out.println("Начислено миль:" + bonus);
+
 
 
     }
-        System.out.
+
 }
