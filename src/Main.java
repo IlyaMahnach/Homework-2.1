1d9647fe9711bdb0f5ec85b53e6317cceb19ed44
IlyaMahnach <ilya_mahnach@mail.ru>
2022-02-08 22:02:30 +0700

    Коммит

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // входные данные
+        int price = 20000;
+        int bid = 20
+        int bonus = price / bid;
+
+
+    }
+        System.out.
+}
